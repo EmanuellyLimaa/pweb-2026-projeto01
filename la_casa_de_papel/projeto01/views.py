84913460e2d108949e8f6a758af6e4b3ea96498f
Emanuelly Lima <emanuellylima1422@gmailo.com>
2026-06-02 17:10:42 -0300

    atualizando a views

--- la_casa_de_papel/projeto01/views.py
+++ la_casa_de_papel/projeto01/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+def inicio(request):
+    return render(request, "time/inicio.html", {"site": site})
+
+def elenco_view(request):
+    return render(request, "time/elenco.html", {"site": site, "elenco": elenco})
+
+def sobre(request):
+    return render(request, "time/sobre.html", {"site": site})
+
 site = {
     "título": "La Casa de Papel",
     "descricao": "Site oficial do elenco da série La Casa de Papel",
