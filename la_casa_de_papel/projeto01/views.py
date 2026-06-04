4f9504e96a33f9b600ec2dc765c6e36c1635b6ad
Emanuelly Maria <emanuellylima1422@gmail.com>
2026-06-03 21:01:57 -0300

    Atualiando a views e o elenco

--- la_casa_de_papel/projeto01/views.py
+++ la_casa_de_papel/projeto01/views.py
@@ -13,20 +13,79 @@ site = {
     "título": "La Casa de Papel",
     "descricao": "Site oficial do elenco da série La Casa de Papel",
     "ano": 2026,
-    "autores": ["Emanuelly Maria", "Almira Beatriz"]
+    "autoras": ["Emanuelly Maria", "Almira Beatriz"]
 }
 
 elenco = [
-    "nome": "Professor", "idade": 45, "posicao": "Líder", "nascimento": "Espanha", "foto": "professor.webp",
-    "nome": "Tóquio", "idade": 34, "posicao": "Narradora", "nascimento": "Brasil", "foto": "tokio.webp",
-    "nome": "Rio", "idade": 25, "posicao": "Hacker", "nascimento": "Brasilo", "foto": "rio.webp",
-    "nome": "Nairobi", "idade": 30, "posicao": "Especialista", "nascimento": "Quênia", "foto": "nairobi.webp",
-    "nome": "Moscú", "idade": 40, "posicao": "Operações", "nascimento": "Rússia", "foto": "moscú.webp",
-    "nome": "Professor", "idade": 45, "posicao": "Líder", "nascimento": "Espanha", "foto": "professor.webp" 
-    "nome": "Helsinki", "idade": 38, "posicao": "Segurança", "nascimento": "Filândia", "foto": "helsinki.webp",
-    "nome": "Denver", "idade": 28, "posicao": "Combater", "nascimento": "Espanha", "foto": "denver.webp",
-    "nome": "Berlim", "idade": 42, "posicao": "Vice-líder", "nascimento": "Alemanhã", "foto": "berlim.webp",
-    "nome": "Arturo", "idade": 50, "posicao": "Refém", "nascimento": "Espanha", "foto": "arturo.webp",
-    "nome": "Raquel", "idade": 35, "posicao": "Inspetora", "nascimento": "Espanha", "foto": "raquel.webp",
-    "nome": "Angel", "idade": 33, "posicao": "Negociador", "nascimento": "Espanha", "foto": "angel.webp",
+    {"nome": "Professor", 
+     "idade": 45, 
+     "posicao": "Líder", 
+     "nascimento": "Espanha", 
+     "foto": "professor.webp"},
+
+    {"nome": "Tóquio", 
+     "idade": 34, 
+     "posicao": "Narradora", 
+     "nascimento": "Brasil", 
+     "foto": "tokio.webp"},
+
+    {"nome": "Rio", 
+     "idade": 25, 
+     "posicao": "Hacker",
+    "nascimento": "Brasilo", 
+    "foto": "rio.webp"},
+
+    {"nome": "Nairobi", 
+     "idade": 30, 
+     "posicao": "Especialista", 
+     "nascimento": "Quênia",
+    "foto": "nairobi.webp"},
+
+    {"nome": "Moscú", 
+     "idade": 40, 
+     "posicao": "Operações", 
+     "nascimento": "Rússia", 
+     "foto": "moscú.webp"},
+
+    {"nome": "Professor", 
+     "idade": 45, 
+     "posicao": "Líder", 
+     "nascimento": "Espanha", 
+     "foto": "professor.webp"},
+
+    {"nome": "Helsinki", 
+     "idade": 38, 
+     "posicao": "Segurança", 
+     "nascimento": "Filândia", 
+     "foto": "helsinki.webp"},
+
+    {"nome": "Denver", 
+     "idade": 28, 
+     "posicao": "Combater", 
+     "nascimento": "Espanha", 
+     "foto": "denver.webp"},
+
+    {"nome": "Berlim", 
+     "idade": 42, 
+     "posicao": "Vice-líder", 
+     "nascimento": "Alemanhã", 
+     "foto": "berlim.webp"},
+
+    {"nome": "Arturo", 
+     "idade": 50, 
+     "posicao": "Refém", 
+     "nascimento": "Espanha", 
+     "foto": "arturo.webp"},
+
+    {"nome": "Raquel", 
+     "idade": 35, 
+     "posicao": "Inspetora", 
+     "nascimento": "Espanha", 
+     "foto": "raquel.webp"},
+
+    {"nome": "Angel", 
+     "idade": 33, 
+     "posicao": "Negociador", 
+     "nascimento": "Espanha", 
+     "foto": "angel.webp"}
 ]
